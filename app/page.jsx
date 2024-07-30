38f3f12e7df647212aaf687089b8979bd6bd52d1
Jeethan Roche <jeethanroche@gmail.com>
2024-07-30 19:58:04 +0530

    Completion of Task 4, added my phone in public folder and imported image component and in root page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Button from "@components/Button";
 import NavBar from '@/components/NavBar';
 import Footer from '@/components/Footer';
+import Image from "next/image";
+import me from "/public/blackme.jpg";
 // import { useNavigate } from 'react-router-dom';
 //task4
 export const page = () => {
@@ -15,6 +17,13 @@ export const page = () => {
         <h1 className="text-4xl md:text-6xl font-bold my-4" style={{ fontFamily: "'Great Vibes', cursive" }}>Jeethan Roche</h1>
       </div>
       <div className="z-10 max-w-5xl w-full text-center font-sans text-sm" style={{ fontFamily: "'Roboto', sans-serif" }}>
+      <Image
+        src={me}
+        alt="code"
+        width={100}
+        height={100}
+        className=" mx-auto items-center h-20"
+/>
         <p className="border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit rounded-xl border bg-gray-200 p-4 dark:bg-zinc-800/30">
           Hey, I am an alumni of St Aloysius College and a day scholar at St Joseph Engineering College. It is so nice to be a part
           of the "Sceptix" Group and also of this Institution. <br />
---
+++ components/Image.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+function MyComponent() {
+    return (
+        <Image 
+            src="/public/blackme.jpg"
+            height={144}
+            width={144}
+            alt="Jeethan Roche image"
+        />
+    )
+}
